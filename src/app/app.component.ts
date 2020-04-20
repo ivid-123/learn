a69d86907e0c9bb9916d0c6c9f492a45feec34b0
ivid-123 <ashish.mishra2@soprasteria.com>
2020-04-20 18:36:18 +0530

    comment added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Letslearn';
+  title = 'Letslearn'; // adding comments
   points = 1;
 
   plus1() {
